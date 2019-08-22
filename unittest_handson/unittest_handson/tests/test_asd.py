78fff4cb21c8f0a3eb035b644715d822a17d2bd9
Roman Geraskin <roman-geraskin@users.noreply.github.com>
2019-08-22 07:23:15 +0300

    pytest

--- unittest_handson/unittest_handson/tests/test_asd.py
+++ unittest_handson/unittest_handson/tests/test_asd.py
@@ -1,18 +1,13 @@
-# from unittest import TestCase
+from unittest import TestCase
 
-import unittest_handson.asd
-from unittest_handson._coverage import CoverageTestCase as TestCase
+from unittest_handson.asd import Asd
 
 
 class CoverageTestAsd(TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.Asd = cls.setUpCoverage(unittest_handson.asd, "Asd")
-
     def test_asd_method(self):
-        asd = self.Asd()
+        asd = Asd()
         self.assertEqual(asd.asd_method(1, 2), 3)
 
     def test_b_method(self):
-        asd = self.Asd()
+        asd = Asd()
         self.assertEqual(asd.b_method(1, 2), 3)
--- unittest_handson/unittest_handson/tests/test_wsd.py
+++ unittest_handson/unittest_handson/tests/test_wsd.py
@@ -1,18 +1,13 @@
-# from unittest import TestCase
+from unittest import TestCase
 
-import unittest_handson.wsd
-from unittest_handson._coverage import CoverageTestCase as TestCase
+from unittest_handson.wsd import Wsd
 
 
 class CoverageTestWsd(TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.Wsd = cls.setUpCoverage(unittest_handson.wsd, "Wsd")
-
     def test_asd_method(self):
-        asd = self.Wsd()
+        asd = Wsd()
         self.assertEqual(asd.asd_method(1, 2), 3)
 
     def test_b_method(self):
-        asd = self.Wsd()
+        asd = Wsd()
         self.assertEqual(asd.b_method(1, 2), 3)
